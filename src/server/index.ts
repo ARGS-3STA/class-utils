a9b3dc3a7db095692f21424de41bcd3ea1356f85
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2023-11-07 19:08:03 +0100

    :sparkles: Add trpc routers for groups and students

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,8 @@
+import { groupRouter } from "@/server/routers/group";
 import { router } from "@/server/trpc";
 
-export const appRouter = router({});
+export const appRouter = router({
+  group: groupRouter,
+});
 
 export type AppRouter = typeof appRouter;
