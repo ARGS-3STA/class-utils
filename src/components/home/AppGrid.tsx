3c0dc089fb924072bdf4dcb2553daa1ba404572d
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2023-11-10 16:48:06 +0100

    :sparkles: Add filter and list components to app-grid

--- src/components/home/AppGrid.tsx
+++ src/components/home/AppGrid.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+"use client";
+
+import { AppFilter } from "@/components/home/app-filter";
+import { AppList } from "@/components/home/app-list";
+import { Separator } from "@/components/ui/separator";
+import { useState } from "react";
 
 export default function AppGrid() {
-  return <div>app-grid</div>;
+  const [filterQuery, setFilterQuery] = useState("");
+
+  return (
+    <div className="container">
+      <AppFilter setFilterQuery={setFilterQuery} />
+      <Separator className="my-4" />
+      <AppList filterQuery={filterQuery} />
+    </div>
+  );
 }
