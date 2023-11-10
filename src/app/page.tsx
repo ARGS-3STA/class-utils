250580935f3d3727f7e46abc3e4f5643f6ff0808
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2023-11-10 16:47:47 +0100

    :art: Cleanup homepage component + comment toolbar (it doesn't work yet)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,15 @@
 "use client";
 
-import { trpcServer } from "@/trpc/serverClient";
-import { Student } from "@prisma/client";
-import { Fragment, useState } from "react";
-import Toolbar from "@/components/layout/Toolbar";
 import AppGrid from "@/components/home/AppGrid";
+import { useState } from "react";
 
 export default function Home() {
   const [menuActive, setMenuActive] = useState(false);
 
   return (
     <div className=" w-screen h-screen">
-      <Toolbar menuActive={menuActive} setMenuActive={setMenuActive}></Toolbar>
-      <AppGrid></AppGrid>
+      {/* <Toolbar menuActive={menuActive} setMenuActive={setMenuActive}></Toolbar> */}
+      <AppGrid />
     </div>
   );
-  /*
-  const groups = await trpcServer.group.getAll({
-    includeStudents: true,
-  });
-
-  return (
-    <div>
-      {groups.map((group: (typeof groups)[number]) => (
-        <Fragment key={group.id}>
-          <p>Id: {group.id}</p>
-          <p>Name: {group.name}</p>
-          <p>Students:</p>
-          {group.students.map((student: Student) => (
-            <Fragment key={student.id}>
-              <p>Student id: {student.id}</p>
-              <p>
-                Student name: {student.firstName} {student.lastName}
-              </p>
-            </Fragment>
-          ))}
-        </Fragment>
-      ))}
-    </div>
-  );
-  */
 }
